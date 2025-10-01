81866f320e4c759dd63ee253d13263f4dac0836f
arieureka2023-del <arieureka2023@gmail.com>
2025-10-01 23:11:14 +0200

    defined the unit step function

--- Signals.py
+++ Signals.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Wed Oct  1 18:49:05 2025
 
-@author: ASUS
-"""
+import numpy as np
+
+# defines the unit-step funtion
+def u (start, finish, amplitude, sample_rate):
+    t=np.linspace (start, finish, sample_rate)
+    return t, np.where (t<0, 0, amplitude)
+
 
