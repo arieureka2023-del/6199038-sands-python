55b5fd833841f8f440ef99ba9f887168f376af72
arieureka2023-del <arieureka2023@gmail.com>
2025-10-01 23:53:26 +0200

    Generated the unit step function

--- Run.py
+++ Run.py
@@ -3,13 +3,21 @@ import Signals as sig
 import matplotlib.pyplot as plt
 
 #generate sine wave function
-t, y = sig.generate_sine_wave (4,2,100)
+t, y = sig.generate_sine_wave (4,2,1000)
 plt.figure (1)
 plt.plot (t,y)
 plt.grid (True)
 plt.title ("Sine wave")
-plt.xlable ("time")
-plt.ylable("amplitude")
+plt.xlabel ("time (s)")
+plt.ylabel("amplitude")
 plt.show()
 
-
+#generate unit step function 
+t, y = sig.u (-10, 10, 3, 10000)
+plt.figure (2)
+plt.plot (t, y)
+plt.grid (True)
+plt.title ("Unit step")
+plt.xlabel ("time (s)")
+plt.ylabel("height (m)")
+plt.show()
