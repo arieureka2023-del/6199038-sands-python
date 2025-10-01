68a9c1086c446bc3aad76ae755d0b9f957d1b3c3
arieureka2023-del <arieureka2023@gmail.com>
2025-10-01 23:12:39 +0200

    defined the sin wave function

--- Signals.py
+++ Signals.py
@@ -7,4 +7,8 @@ def u (start, finish, amplitude, sample_rate):
     t=np.linspace (start, finish, sample_rate)
     return t, np.where (t<0, 0, amplitude)
 
-
+# defines sine wave function
+def generate_sine_wave (frequency, duration, sample_rate):
+    t= np.linspace (0, duration, sample_rate)
+    y= np.sin (frequency*2*np.pi*t)
+    return t, y
