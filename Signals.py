6d24dcaf9831e1fb66ae95b563e3095414ebd9e7
arieureka2023-del <arieureka2023@gmail.com>
2025-10-03 19:18:23 +0200

    consistency mistake fixed

--- Signals.py
+++ Signals.py
@@ -3,7 +3,7 @@
 import numpy as np
 
 # defines the unit-step funtion
-def u (start, finish, amplitude, sample_rate):
+def generate_u (start, finish, amplitude, sample_rate):
     t= np.linspace (start, finish, sample_rate)
     return t, np.where (t<0, 0, amplitude)
 
