ef787268d6859d852a113e272cf15933594fd68d
arieureka2023-del <arieureka2023@gmail.com>
2025-11-07 18:47:55 +0100

    fixed consistency in the different functions

--- test.py
+++ test.py
@@ -21,23 +21,33 @@ def test_generate_sine_wave():
 #testing modified_sine_wave function
 def test_modified_sine_wave():
     t, y = sig.modified_sine_wave(5, 2, 10000, amplitude=10.0)
-    assert len(t) == 10000
+    
     assert y[0] == 0 
+    
+    assert len(t) == 10000
+    
     assert np.allclose(max(y), 10, atol=1e-6)
     
 #testing generate_u function
 def test_generate_u():
     t, y= sig.generate_u(-10,10,3,10000)
-    assert len(t) == 10000
-    assert np.allclose(max(y),3, atol=1e-6)
+   
     assert y[0] == 0
-    assert y[9000] == 3
+    assert y[9000] == 3 
+    
+    assert len(t) == 10000
+   
+    assert np.allclose(max(y),3, atol=1e-6)
     
 #testing modified_u function
 def test_modified_u():
     t, y= sig.modified_u(-10,10,-2,3,10000)
-    assert len(t) == 10000
-    assert np.allclose(max(y), 3, atol=1e-6)
+    
     assert y[0] == 0
     assert y[9000] == 3
+    
+    assert len(t) == 10000
+    
+    assert np.allclose(max(y), 3, atol=1e-6)
+
  
